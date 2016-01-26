cd5e2a2750c460aae665b5e2a0d6f3c4b070bba7
Marcos Oliveira <marcosf63@gmail.com>
2016-01-26 16:54:11 -0300

    Update

---
+++ config.py
@@ -0,0 +1,36 @@
+import os
+basedir = os.pathabspath(os.path.dirname(__file__))
+
+class Config:
+  SECRET_KEY                    = os.environ.get('SECRET_KEY')
+  SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+  FLASK_MAIL_SUBJECT_PREFIX     = '[Flask]'
+  FLASK_MAIL_SENDER             = 'Flask Admin <flask@xemple.com>'
+  FLASK_ADMIN                   = os.environ.get('FLASK_ADMIN')
+
+  @staticmethod
+  def init_app(app):
+    pass
+
+class DevelopmentConfig(Config):
+  DEBUG = True
+  MAIL_SERVER             = 'smtp.googlemail.com'
+  MAIL_PORT               = 587
+  MAIL_USE_TLS            = True
+  MAIL_USERNAME           = os.environ.get('MAIL_USERNAME')
+  MAIL_PASSWORD           = os.environ.get('MAIL_PASSWORD')
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL')
+
+class TestingConfig(Config):
+  TESTING = True
+  SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL')
+
+class ProductionConfig(Config):
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+config = {
+  'development' : DevevopmentConfig,
+  'testing'     : TestingConfig,
+  'production'  : ProductionConfig,
+  'default'     : DevelopmentConfig
+}
